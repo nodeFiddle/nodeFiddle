f5360c1f00029e26b0277461221401f8f15e2613
Franco Arolfo <francoarolfo@hotmail.com>
2015-06-02 18:10:10 -0300

    added ace editor

--- app/home/homeCtrl.js
+++ app/home/homeCtrl.js
@@ -3,5 +3,7 @@
 angular.module('nodeFiddle.controllers')
 
 .controller('HomeCtrl', ['$scope', '$state', function($scope, $state) {
-
+  var editor = ace.edit("editor");
+  editor.setTheme("ace/theme/monokai");
+  editor.getSession().setMode("ace/mode/javascript");
 }]);
